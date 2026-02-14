dabb877798de8943053b051cf49981f8e265cec8
arath abello <arathabello60@gmail.com>
2026-02-14 13:07:21 -0300

    empezando javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+let precio = 400000
+
+let precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+let cantidad = document.querySelector(".cantidad");
+let sumaBtn = document.querySelector('.sumar');
+let restarBtn = document.querySelector('.restar');
+let valorTotal = document.querySelector('.valor-total');
+
+
